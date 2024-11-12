7fa5b04b25912903047293dcd8215b547b099fcd
thesatelliteever <165311591+thesatelliteever@users.noreply.github.com>
2024-11-12 09:44:29 -0500

    Update sniper.js

--- sniper.js
+++ sniper.js
@@ -11,40 +11,18 @@ javascript:(function() {
 	cooldown: 0,
         fire() {
             if (this.canSRFire) {
-                b.explosion(simulation.mouseInGame, 10);
+                b.explosion(simulation.mouseInGame, 100);
 		this.canSRFire = false;
-	    } else {
-		this.cooldown++;
-		if (this.cooldown > 60) {
-		    this.cooldown = 0;
-		    this.canSRFire = true;
-		};
 	    };
         },
 	do() {
-                //fade cross hairs
-
-
-
-                // draw loop around player head
-                // const left = m.fireCDcycle !== Infinity ? 0.05 * Math.max(m.fireCDcycle - m.cycle, 0) : 0
-                // if (left > 0) {
-                //     ctx.beginPath();
-                //     // ctx.arc(simulation.mouseInGame.x, simulation.mouseInGame.y, 30, 0, left);
-                //     ctx.arc(m.pos.x, m.pos.y, 28, m.angle - left, m.angle);
-                //     // ctx.fillStyle = "rgba(0,0,0,0.3)" //"#333"
-                //     // ctx.fill();
-                //     ctx.strokeStyle = "#333";
-                //     ctx.lineWidth = 2;
-                //     ctx.stroke();
-                // }
-
-
-                //draw hip circle
-                // ctx.beginPath();
-                // ctx.arc(m.pos.x + m.hip.x, m.pos.y + m.hip.y, 11, 0, 2 * Math.PI);
-                // ctx.fillStyle = "rgba(0,0,0,0.3)" //"#333"
-                // ctx.fill();
+                if (!this.canSRFire) {
+		    this.cooldown++;
+		    if (this.cooldown > 60) {
+		        this.cooldown = 0;
+		        this.canSRFire = true;
+	    	    };
+		};
         },
     };       
 	b.guns.push(e);
