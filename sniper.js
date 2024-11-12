2d2dbdc0191f6350ddbd9a37e86747524ae26c94
thesatelliteever <165311591+thesatelliteever@users.noreply.github.com>
2024-11-12 09:35:23 -0500

    Update sniper.js

--- sniper.js
+++ sniper.js
@@ -22,8 +22,30 @@ javascript:(function() {
 	    };
         },
 	do() {
+                //fade cross hairs
 
-	}
+
+
+                // draw loop around player head
+                // const left = m.fireCDcycle !== Infinity ? 0.05 * Math.max(m.fireCDcycle - m.cycle, 0) : 0
+                // if (left > 0) {
+                //     ctx.beginPath();
+                //     // ctx.arc(simulation.mouseInGame.x, simulation.mouseInGame.y, 30, 0, left);
+                //     ctx.arc(m.pos.x, m.pos.y, 28, m.angle - left, m.angle);
+                //     // ctx.fillStyle = "rgba(0,0,0,0.3)" //"#333"
+                //     // ctx.fill();
+                //     ctx.strokeStyle = "#333";
+                //     ctx.lineWidth = 2;
+                //     ctx.stroke();
+                // }
+
+
+                //draw hip circle
+                // ctx.beginPath();
+                // ctx.arc(m.pos.x + m.hip.x, m.pos.y + m.hip.y, 11, 0, 2 * Math.PI);
+                // ctx.fillStyle = "rgba(0,0,0,0.3)" //"#333"
+                // ctx.fill();
+        },
     };       
 	b.guns.push(e);
 	const gunArray = b.guns.filter(
