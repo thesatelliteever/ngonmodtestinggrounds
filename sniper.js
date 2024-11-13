45ba72a89095e23c880597aabafc58647fd94ec8
thesatelliteever <165311591+thesatelliteever@users.noreply.github.com>
2024-11-13 09:07:45 -0500

    Update sniper.js

--- sniper.js
+++ sniper.js
@@ -9,15 +9,15 @@ javascript:(function() {
         cycle: 0,
         canSRFire: true,
 	cooldown: 0,
-	tech.SRLength: 60,
-	tech.SRRadius: 50,
-	tech.SRPower: 10,
+	cdLength: 60,
+	shotRadius: 50,
+	shotPower: 10,
         fire() {
 	    this.ammo++;
             if (this.canSRFire) {
 		this.ammo--;
-                for (let index = 0; index < this.SRPower; index++) {
-    			b.explosion(simulation.mouseInGame, this.SRRadius);
+                for (let index = 0; index < this.shotPower; index++) {
+    			b.explosion(simulation.mouseInGame, this.shotRadius);
 		};
 		this.canSRFire = false;
 	    };
@@ -25,72 +25,23 @@ javascript:(function() {
 	do() {
                 if (!this.canSRFire) {
 		    this.cooldown++;
-		    if (this.cooldown > this.SRLength) {
+		    if (this.cooldown > this.cdLength) {
 		        this.cooldown = 0;
 		        this.canSRFire = true;
 	    	    };
 		};
         },
     };       
-    b.guns.push(e);
-    const gunArray = b.guns.filter(
-        (obj, index, self) =>
-	    index === self.findIndex((item) => item.name === obj.name)
-    );
-    const t = [
-		{
-			name: "argon scope",
-			descriptionFunction() {
-				return `<strong>+100%</strong> sniper rifle fire rate, but  <strong>-50%</strong> sniper rifle damage`
-			},
-			isGunTech: true,
-			maxCount: 3,
-			count: 0,
-			frequency: 2,
-			frequencyDefault: 2,
-			allowed() {
-				return tech.haveGunCheck("sniper rifle")
-			},
-			requires: "sniper rifle",
-			effect() {
-				tech.SRLength *= 0.5;
-				tech.SRRadius *= 0.5;
-			},
-			remove() {
-				tech.SRLength *= 2;
-				tech.SRRadius *= 2;
-			}
-		},
-		{
-			name: "placeholder name",
-			descriptionFunction() {
-				return `<strong>+100%</strong> sniper rifle damage, but  <strong>-50%</strong> sniper rifle fire rate`
-			},
-			isGunTech: true,
-			maxCount: 3,
-			count: 0,
-			frequency: 2,
-			frequencyDefault: 2,
-			allowed() {
-				return tech.haveGunCheck("sniper rifle")
-			},
-			requires: "sniper rifle",
-			effect() {
-				tech.SRLength *= 2;
-				tech.SRPower *= 2;
-			},
-			remove() {
-				tech.SRLength *= 0.5;
-				tech.SRPower *= 0.5;
-			}
-		},
-    ];
-    tech.tech.push(t);
-    const techArray = tech.tech.filter(
-        (obj, index, self) =>
-    	    index === self.findIndex((item) => item.name === obj.name)
-    );
-    b.guns = gunArray;
-    tech.tech = techArray;
-    console.log("%cSniper mod successfully installed", "color: gray");
+	b.guns.push(e);
+	const gunArray = b.guns.filter(
+	(obj, index, self) =>
+		index === self.findIndex((item) => item.name === obj.name)
+	);
+	const techArray = tech.tech.filter(
+	(obj, index, self) =>
+		index === self.findIndex((item) => item.name === obj.name)
+	);
+	b.guns = gunArray;
+	tech.tech = techArray;
+	console.log("%cSniper mod successfully installed", "color: gray");
 })();
